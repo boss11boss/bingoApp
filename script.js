b17b1c154bec4b679a32f82f12b2bd44321b4409
boss11boss <agrawalvaibhav199@gmail.com>
2021-05-22 00:28:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.from('#nav h3',{x:80, duration:1,opacity:0,ease:'expo.inOut'})
+
+gsap.from('#links a',{x:80,stagger:.2, duration:1,delay:.2,opacity:0,ease:'expo.inOut'})
+
+gsap.from('#sidelinks a',{x:80,delay:.8, duration:1.5,opacity:0,ease:'expo.inOut'})
+
+gsap.from('#aa',{x:80,delay:1, duration:1.5,opacity:0,ease:'expo.inOut'})
+
+gsap.from('#foot img',{y:90,stagger:.3,delay:.5, duration:1.3,ease:'expo.inOut' , opacity:0})
+
+gsap.from('#right img',{x:'100%',delay:.4, duration:2.2,ease:'expo.inOut'})
+
+gsap.from('#right img',{scale:2,delay:1.3, duration:1.9,ease:'expo.inOut'})
+
+gsap.from('.bb',{delay:.5, duration:1.7,ease:'expo.inOut',y:80,stagger:.2,opacity:0})
+
+gsap.from('.line',{delay:1.8, duration:1.4,ease:'expo.inOut',width:'0%',opacity:0})
